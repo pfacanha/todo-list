1d1a89c2367365e2e33226d6f60714301c08785e
pfacanha <pfacanha.dev@gmail.com>
2026-01-28 07:27:01 -0800

    Create createTodo() and addTodo()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "./styles.css";
 
+const todos = [];
 class Todo {
   constructor(title, description, dueDate, priority, checklist) {
+    this.id = crypto.randomUUID();
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
@@ -10,6 +12,16 @@ class Todo {
   }
 }
 
-function addTodo() {}
+function createTodo(title, description, dueDate, priority) {
+  return new Todo(title, description, dueDate, priority, false);
+}
+function addTodo(todo) {
+  todos.push(todo);
+}
 function deleteTodo() {}
 function toggleTodo() {}
+
+const newTodo = createTodo("Surfar", "In Sombrio", "Today", "High");
+addTodo(newTodo);
+
+console.log(todos);
