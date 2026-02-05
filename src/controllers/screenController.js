acd6dcc7ecb917e8edfbba912181a7b4d90de2cc
pfacanha <pfacanha.dev@gmail.com>
2026-02-05 09:02:41 -0800

    Add dialog box to addForm and configure it accordingly

--- src/controllers/screenController.js
+++ src/controllers/screenController.js
@@ -1,3 +1,12 @@
 import Todo from "../models/Todo.js";
 import Project from "../models/Project.js";
 import todoController from "../controllers/todoController.js";
+
+const addForm = document.querySelector(".addForm");
+const addBtn = document.querySelector(".addBtn");
+
+addBtn.addEventListener("click", function () {
+  const dialog = document.createElement("dialog");
+
+  addForm.setAttribute("method", "dialog");
+});
