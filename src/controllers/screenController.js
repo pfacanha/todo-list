61073b6d50d937eae61a3b1de739f73962e2654a
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-02-11 17:12:32 -0800

    Test removing addTodo button and adding <div> with input and confirm and cancel buttons

--- src/controllers/screenController.js
+++ src/controllers/screenController.js
@@ -4,5 +4,19 @@ import todoController from "../controllers/todoController.js";
 
 const addForm = document.querySelector(".addForm");
 const addTodo = document.querySelector(".addTodo");
+const addProject = document.querySelector(".addProject");
 
-addTodo.addEventListener("click", function () {});
+addTodo.addEventListener("click", function () {
+  addTodo.remove();
+
+  // create input
+  const input = document.createElement("input");
+  const confirmBtn = document.createElement("button");
+  const cancelBtn = document.createElement("button");
+
+  const div = document.createElement("div");
+  div.append(input, confirmBtn, cancelBtn);
+
+  addForm.appendChild(div);
+});
+addProject.addEventListener("click", function () {});
