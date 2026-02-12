d3a093db2ce5bc13d34eca78e5f4984db5f8b101
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-02-11 16:39:54 -0800

    Start implementing todo to projects or not

--- src/controllers/screenController.js
+++ src/controllers/screenController.js
@@ -3,9 +3,10 @@ import Project from "../models/Project.js";
 import todoController from "../controllers/todoController.js";
 
 const addForm = document.querySelector(".addForm");
-const addBtn = document.querySelector(".addBtn");
+const addTodo = document.querySelector(".addTodo");
+const addProject = document.querySelector(".addProject");
 
-addBtn.addEventListener("click", function () {
+addTodo.addEventListener("click", function () {
   const dialog = document.createElement("dialog");
   const p = document.createElement("p");
   p.textContent = `Would you like to add this todo to a project?`;
