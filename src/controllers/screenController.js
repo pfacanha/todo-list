a07afa307507dd3f83436a1edcacd6d8eae5f69c
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-02-11 16:45:40 -0800

    Implement addBtn event listener function

--- src/controllers/screenController.js
+++ src/controllers/screenController.js
@@ -4,16 +4,5 @@ import todoController from "../controllers/todoController.js";
 
 const addForm = document.querySelector(".addForm");
 const addTodo = document.querySelector(".addTodo");
-const addProject = document.querySelector(".addProject");
 
-addTodo.addEventListener("click", function () {
-  const dialog = document.createElement("dialog");
-  const p = document.createElement("p");
-  p.textContent = `Would you like to add this todo to a project?`;
-  dialog.appendChild(p);
-
-  addForm.setAttribute("method", "dialog");
-  addForm.appendChild(dialog);
-
-  dialog.showModal();
-});
+addTodo.addEventListener("click", function () {});
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import todoController from "./controllers/todoController.js";
 import screenController from "./controllers/screenController.js";
 
